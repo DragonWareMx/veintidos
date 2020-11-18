0c64a1f8138699bd5859575712915e411509bb69
PapaDrago <oscarwaii@hotmail.com>
2020-11-18 14:00:24 -0600

    ya empecé jiji
    tengo pedos mentales

---
+++ resources/views/notFound.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.publi')
+@section('title')
+    Sin Resultados
+@endsection
+@section('head')
+    <link rel="stylesheet" href="{{ asset('/css/O.css') }}">
+@endsection
+@section('content')
+    <div class="Odiv100">
+        <div class="Otitle"><h1>PROPIEDADES | </h1><h2>CASAS</h2></div>
+        <div class="OGreenText OBR70">No se encontraron resultados</div>
+    </div>
+@endsection
