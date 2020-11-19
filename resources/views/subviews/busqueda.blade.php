59d92a84e3bfc3c3338dd1370c62c8c44bd756c1
leoDlopez <lopez_lopez_daniel@hotmail.com>
2020-11-19 13:58:17 -0600

    Avances en la barra de busqueda
    Falta responsividad

--- resources/views/subviews/busqueda.blade.php
+++ resources/views/subviews/busqueda.blade.php
@@ -1,4 +1,17 @@
 <form action="" class="barra-busqueda" action="">
     @csrf
-    
+    <div class="search-50 left">
+        <select name="tipo" id="" class="options-search">
+            <option value="" selected disabled>Tipo</option>
+        </select>
+        <select name="precio" id="" class="options-search">
+            <option value="" selected disabled>Precio</option>
+        </select>
+        <button type="button" class="type-search options-search">Venta</button>
+        <button type="button" class="type-search options-search">Renta</button>
+    </div>
+    <div class="search-50 right">
+        <input type="text" name="busqueda" id="busqueda" class="cuadro-busqueda options-search">
+        <input type="image" src="{{ asset('/img/ico/buscar.png') }}" alt="Submit Form" class="search-button" />
+    </div>
 </form>
