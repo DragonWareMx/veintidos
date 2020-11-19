4bb442f6aae403fe29778eb95d28c4edc40880c2
PapaDrago <oscarwaii@hotmail.com>
2020-11-18 23:40:26 -0600

    ira dei
    ira de dios

--- resources/views/notFound.blade.php
+++ resources/views/notFound.blade.php
@@ -9,5 +9,28 @@
     <div class="Odiv100">
         <div class="Otitle"><h1>PROPIEDADES | </h1><h2>CASAS</h2></div>
         <div class="OGreenText OBR70">No se encontraron resultados</div>
+        <div class="OBR70 OContactUs">
+            <h1>CONTÁCTANOS</h1>
+            <div class="OContactUsItem">
+                <img class="OContactUsImg" src="{{ asset('img/ico/phone.png') }}" alt="">
+                <div class="OContactUsTitle">Teléfono</div>
+            </div>
+            <div class="OContactUsItem">
+                <div class="OContactUsValue">4433370550</div>
+            </div>
+            <div class="OContactUsItem">
+                <img class="OContactUsImg" src="{{ asset('img/ico/email.png') }}" alt="">
+                <div class="OContactUsTitle">Correo electrónico</div>
+            </div>
+            <div class="OContactUsItem">
+                <div class="OContactUsValue">contacto@veintidos.mx</div>
+            </div>
+        </div>
+        <div class="OWidth100 OCenterItem"> 
+            <a href="#" class="OButtonInicio">
+                <div class="OButtonInicioText">INICIO</div>
+                <img class="OButtonInicioArrow" src="{{ asset('img/ico/next-white.png') }}" alt="">
+            </a>
+        </div>  
     </div>
 @endsection
