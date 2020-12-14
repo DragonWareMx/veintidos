02a5e4c6847696c9667510f17d6b2ce3d6e30912
FernandoGASA <66817120+FernandoGASA@users.noreply.github.com>
2020-12-14 16:57:14 -0600

    owl

--- resources/views/Propiedades/propiedad.blade.php
+++ resources/views/Propiedades/propiedad.blade.php
@@ -5,6 +5,12 @@
 @section('head')
     <link rel="stylesheet" href="{{ asset('/css/O.css') }}">
     <link rel="stylesheet" href="{{ asset('/plugins/bootstrap/css/bootstrap.css') }}">
+
+    {{-- OWL CAROUSEL --}}
+    <link rel="stylesheet" href="{{ asset('/plugins/OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/plugins/OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css') }}">
+
+    <script src="{{ asset('/plugins/OwlCarousel2-2.3.4/dist/owl.carousel.min.js') }}"></script>
 @endsection
 @section('content')
     <div class="container">
@@ -15,6 +21,33 @@
                 <div class="row mt-4">
                     {{-- AQUI VA LA GALERIA DE FOTOS --}}
                     <div class="col">
+                            <div class="w-100 h-100">
+                                {{-- IMAGEN SELECCIONADA --}}
+                                <div class="mx-auto bg-dark" style="height: 400px; width: 95%;">
+                                </div>
+
+                                {{-- Carousel --}}
+                                <div class="mx-auto mt-2" style="width: 95%;">
+                                    <ul class="owl-carousel owl-theme">
+                                        <li>
+                                            <a href="http://placehold.it/1280x720" data-size="1280x720"><img  src="http://placehold.it/1280x720" alt="1" title="Imagen 1"></a>
+                                        </li>
+                                        <li>
+                                            <a href="http://placehold.it/720x1280" data-size="720x1280"><img  src="http://placehold.it/720x1280" style="height:70px;" alt="2" title="Imagen 2"></a>
+                                        </li>
+                                        <li>
+                                            <a href="http://placehold.it/1920x1920" data-size="1920x1920">
+                                              <img  src="http://placehold.it/400x300" alt="3">
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="http://placehold.it/960x640" data-size="960x640">
+                                              <img  src="http://placehold.it/400x300" alt="4">
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                     </div>
                     {{-- DATOS DE LA PROPIEDAD --}}
                     <div class="col">
@@ -119,4 +152,27 @@
             {{-- AQUI VA EL CARRUSEL DE LAS PROPIEDADES --}}
         </div>
     </div>
+
+    <script>
+        $(document).ready(function(){
+            $(".owl-carousel").owlCarousel({
+                loop:false,
+                margin:10,
+                nav:true,
+                autoWidth:false,
+                autoHeight:false,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:3
+                    },
+                    1000:{
+                        items:5
+                    }
+                }
+            });
+        });
+    </script>
 @endsection
