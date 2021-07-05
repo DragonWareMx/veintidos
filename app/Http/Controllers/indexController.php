b8eefb778b695b47e3940372f4b0caf861174224
FernandoGASA <xpadrian2010@hotmail.com>
2021-07-05 13:35:51 -0500

    sin preloader y limitado el carrusel

--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -8,7 +8,7 @@ use App\Propertie;
 class indexController extends Controller
 {
     public function index(Request $request){
-        $propiedades=Propertie::orderby('created_at','desc')->limit(20)->get();
+        $propiedades=Propertie::where('status','available')->orderby('created_at','desc')->limit(10)->get();
         return view('test',['propiedades'=>$propiedades]);
     }
 }
